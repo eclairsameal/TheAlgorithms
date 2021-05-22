ac7c9678a30fbbc7a7900af340998ca27c5eed16
Fenrir <fenrireclair@gmail.com>
2021-05-23 05:17:40 +0900

    新增 print Linked list function

--- linked list/Group_Linked_list.py
+++ linked list/Group_Linked_list.py
@@ -2,4 +2,10 @@ import Group as g
 class Group_Linked_list():
     def __init__(self):
         self.head = None
- 
+    
+    def print_list():
+        """print Linked list"""
+        ptr = self.head
+        while ptr:
+            print(ptr.name)
+            ptr = ptr.next
