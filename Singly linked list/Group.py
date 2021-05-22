4c600eb1eec57113532d5ef3a419160a732c1b4a
Fenrir <>
2021-05-22 19:05:33 +0000

    實作節點

--- Singly linked list/Group.py
+++ Singly linked list/Group.py
@@ -1,5 +1,23 @@
 class Group():
   """節點"""
-  def __init__(self, name = Node):
+  def __init__(self, name = None):
     self.data = name
-    self.next = Node
+    self.next = None
+
+# 實作節點
+
+illumina = Group("illumination  STARS")
+antica = Group("L'Antica")
+alstroe = Group("ALSTROEMERIA")
+hokako = Group("放課後クライマックスガールズ")
+
+illumina.next = antica
+antica.next = alstroe
+alstroe.next = hokako
+
+ptr = illumina # 建立指標節點
+
+# 遊歷節點
+while ptr:
+  print(ptr)
+  ptr = ptr.next
