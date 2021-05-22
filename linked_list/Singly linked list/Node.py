0887b6b6435d793f45472e61336380e7773fa544
Fenrir <>
2021-05-22 18:48:18 +0000

    修正class的主題

--- linked_list/Singly linked list/Node.py
+++
@@ -1,5 +0,0 @@
-class Node():
-  """節點"""
-  def __init__(self, data = Node):
-    self.data = data
-    self.next = Node
---
+++ linked_list/Singly linked list/ShinyColorsGroup.py
@@ -0,0 +1,5 @@
+class ShinyColorsGroup():
+  """節點"""
+  def __init__(self, name = Node):
+    self.data = name
+    self.next = Node
