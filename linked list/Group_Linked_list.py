7a004a7ad3e86e8f325b0328f59f741bc1f5bf28
Fenrir <fenrireclair@gmail.com>
2021-05-29 04:40:33 +0900

    新增function(將新的節點插入到Linked list的中間)

--- linked list/Group_Linked_list.py
+++ linked list/Group_Linked_list.py
@@ -26,3 +26,12 @@ class Group_Linked_list():
         while last_ptr.next:         # 移動最後節點的指標直到Linked list的最後
             last_ptr = last_ptr.next
         last_ptr.next = new_group    # 將最後節點的next 指向新的節點
+    
+    def add_between(self, pre_node, new_name):
+        """將新的節點插入到Linked list的中間"""
+        if pre_node == None:
+            print("沒有前一個節點")
+            return
+        new_group = g.Group(new_name)    # 先建立新的節點
+        new_group.next = pre_node.next   # 新的節點先指向後面的節點
+        pre_node.next = new_group        # 將前節點的next指向新的節點
