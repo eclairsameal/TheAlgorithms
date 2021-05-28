c5ea7227ab6d24db476ca01da489d72739a444b2
Fenrir <fenrireclair@gmail.com>
2021-05-29 04:07:26 +0900

    "noctchill" 到最後一個節點(實作 add_ending(self, new_name))

--- linked list/ShinyColors.py
+++ linked list/ShinyColors.py
@@ -1,7 +1,6 @@
 import Group_Linked_list as gll
 import Group as g
 
-
 group_linked = gll.Group_Linked_list()    # 宣告 Group_Linked_list 變數
 group_linked.head = g.Group("illumination  STARS")    # 設定 Group_Linked_list 變數 head的值
 #print(group_linked.head.name)
@@ -17,9 +16,18 @@ print("初始的Linked list: ")
 group_linked.print_list()    # 印出 Group_Linked_list 的所有數值
 
 """
----------- 實作add_beginning(self, new_name) ----------
+---------- 實作 add_beginning(self, new_name) ----------
 新增 "Straylight" 到第一個節點
 """
 print("新增 Straylight 到第一個節點後的Linked list: ")
 group_linked.add_beginning("Straylight")
 group_linked.print_list()
+
+"""
+---------- 實作 add_ending(self, new_name) ----------
+新增 "noctchill" 到最後一個節點
+"""
+print("新增 noctchill 到第一個節點後的Linked list: ")
+new_group_linked = gll.Group_Linked_list()    # 宣告 Group_Linked_list 變數
+new_group_linked.add_ending("noctchill")
+new_group_linked.print_list()
