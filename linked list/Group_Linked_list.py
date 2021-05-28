e8447945d62484e84f9c45c22c84572f11b61c71
Fenrir <fenrireclair@gmail.com>
2021-05-29 03:56:54 +0900

    追加將新的節點追加到Linked list最後面的function

--- linked list/Group_Linked_list.py
+++ linked list/Group_Linked_list.py
@@ -12,6 +12,17 @@ class Group_Linked_list():
     
     def add_beginning(self, new_name):
         """將新的節點插入第一個節點"""
-        new_group = g.Group(new_name)
+        new_group = g.Group(new_name)    # 先建立新的節點
         new_group.next = self.head    # 將原本的節點接到新的節點後
-        self.head = new_group    # 將新的節點設為head
+        self.head = new_group         # 將新的節點設為head
+
+    def add_ending(self, new_name):
+        """將新的節點追加到Linked list最後面"""
+        new_group = g.Group(new_name)    # 先建立新的節點
+        if self.head == None:        # 如果為True的話，表示Linked list是空的
+            self.head = new_group    # 直接將head指向新的節點
+            return
+        last_ptr = self.head         # 先將最後節點的指標指向hand
+        while last_ptr.next:         # 移動最後節點的指標直到Linked list的最後
+            last_ptr = last_ptr.next
+        last_ptr.next = new_group    # 將最後節點的next 指向新的節點
