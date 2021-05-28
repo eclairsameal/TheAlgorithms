dbccb185e69cde78a585931de2b6b8215bbe385f
Fenrir <fenrireclair@gmail.com>
2021-05-29 03:13:25 +0900

    修改註解風格

--- linked list/ShinyColors.py
+++ linked list/ShinyColors.py
@@ -1,19 +1,17 @@
 import Group_Linked_list as gll
 import Group as g
 
-# 宣告 Group_Linked_list 變數
-group_linked = gll.Group_Linked_list()
-# 設定 Group_Linked_list 變數 head的值
-group_linked.head = g.Group("illumination  STARS")
+
+group_linked = gll.Group_Linked_list()    # 宣告 Group_Linked_list 變數
+group_linked.head = g.Group("illumination  STARS")    # 設定 Group_Linked_list 變數 head的值
 #print(group_linked.head.name)
-# 新增 3 個Group節點
-antica = g.Group("L'Antica")
+
+antica = g.Group("L'Antica")    # 新增 3 個Group節點
 alstroe = g.Group("ALSTROEMERIA")
 hokako = g.Group("放課後クライマックスガールズ")
-# 將Group節點連接一起
-antica.next = alstroe
+antica.next = alstroe    # 將Group節點連接一起
 alstroe.next = hokako
-# 將新的Group節點串列接到head後
-group_linked.head.next = antica
-# 印出 Group_Linked_list 的所有數值
-group_linked.print_list()
+
+group_linked.head.next = antica    # 將新的Group節點串列接到head後
+
+group_linked.print_list()    # 印出 Group_Linked_list 的所有數值
